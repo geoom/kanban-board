def20001ef66eb58cf5f535decc24f7d03288538
geoom <hi.geoom@gmail.com>
2016-01-20 21:59:04 -0500

    add local storage

--- app/index.jsx
+++ app/index.jsx
@@ -4,6 +4,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
 
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
+
+persist(alt, storage, 'app');
+
 ReactDOM.render(
 	<App />, 
 	document.getElementById('app')
