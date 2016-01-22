9ba662f381b91bcef456f3739c699e61ebae0b57
geoom <hi.geoom@gmail.com>
2016-01-22 13:07:11 -0500

    add persistency handler, update storage

--- app/libs/storage.js
+++ app/libs/storage.js
@@ -1,17 +1,13 @@
-import makeFinalStore from 'alt-utils/lib/makeFinalStore';
-
-export default function(alt, storage, storeName) {
-  const finalStore = makeFinalStore(alt);
-
-  try {
-    alt.bootstrap(storage.get(storeName));
-  }catch(e) {
-    console.error('Failed to bootstrap data', e);
-  }
-  
-  finalStore.listen(() => {
-    if(!storage.get('debug')) {
-      storage.set(storeName, alt.takeSnapshot());
+export default {
+  get(k) {
+    try {
+      return JSON.parse(localStorage.getItem(k));
     }
-  });
-}
+    catch(e) {
+      return null;
+    }
+  },
+  set(k, v) {
+    localStorage.setItem(k, JSON.stringify(v));
+  }
+};
