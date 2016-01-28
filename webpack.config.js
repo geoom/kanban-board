13b45d34f73b3a6d7e61b405ca5a50cf01bcfa9f
geoom <hi.geoom@gmail.com>
2016-01-28 18:45:20 -0500

    add statistics complement

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,10 @@ const merge = require('webpack-merge');
 // Load *package.json* so we can use `dependencies` from there
 const pkg = require('./package.json');
 
+const Clean = require('clean-webpack-plugin');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 require('es6-promise').polyfill();
 
 const TARGET = process.env.npm_lifecycle_event;
@@ -45,13 +49,6 @@ const common = {
 	],
 	module: {
 	    loaders: [
-			{
-				// Test expects a RegExp! Note the slashes!
-				test: /\.css$/,
-				loaders: ['style', 'css'],
-				// Include accepts either a path or an array of paths.
-				include: PATHS.app
-			},
 			// Set up jsx. This accepts js too thanks to RegExp
 			{
 				test: /\.jsx?$/,
@@ -64,7 +61,6 @@ const common = {
 	    ]
   	},
 };
-
 
 
 // Default configuration
@@ -84,13 +80,23 @@ if(TARGET === 'start' || !TARGET) {
       host: process.env.HOST,
       port: process.env.PORT
     },
+    module: {
+      loaders: [
+        // Define development specific CSS setup
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: PATHS.app
+        }
+      ]
+    },
     plugins: [
       new webpack.HotModuleReplacementPlugin()
     ]
   });
 }
 
-if(TARGET === 'build') {
+if(TARGET === 'build' || TARGET === 'stats') {
   module.exports = merge(common, {
   	// Define entry points needed for splitting
     entry: {
@@ -107,7 +113,20 @@ if(TARGET === 'build') {
       filename: '[name].[chunkhash].js',
       chunkFilename: '[chunkhash].js'
     },
+    module: {
+      loaders: [
+        // Define development specific CSS setup
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: PATHS.app
+        }
+      ]
+    },
     plugins: [
+      // Output extracted CSS to a file
+      new ExtractTextPlugin('styles.[chunkhash].css'),
+      new Clean([PATHS.build]),
       // Extract vendor and manifest files
       new webpack.optimize.CommonsChunkPlugin({
         names: ['vendor', 'manifest']
